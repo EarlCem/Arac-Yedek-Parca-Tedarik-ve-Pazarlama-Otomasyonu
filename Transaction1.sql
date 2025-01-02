b0afa09c8c662d930db3db47f2d24806bda6191f
EarlCem <57097066+EarlCem@users.noreply.github.com>
2025-01-02 16:11:00 +0300

    Transaction

---
+++ Transaction1.sql
@@ -0,0 +1,21 @@
+﻿SELECT * FROM Ürün;
+
+BEGIN TRANSACTION;
+
+BEGIN TRY
+    UPDATE Ürün
+    SET Stok_Miktari = Stok_Miktari -3
+    WHERE Parca_ID = 322;
+
+    UPDATE Ürün
+    SET Stok_Miktari = Stok_Miktari -15
+    WHERE Parca_ID = 323;
+
+    COMMIT TRANSACTION;
+    PRINT 'Tüm işlemler başarıyla tamamlandı.';
+END TRY
+
+BEGIN CATCH
+    ROLLBACK TRANSACTION;
+    PRINT 'Hata oluştu: İşlemler geri alındı.';
+END CATCH;
---
+++ Transaction2.sql
@@ -0,0 +1,32 @@
+﻿BEGIN TRANSACTION;
+
+BEGIN TRY
+
+    DECLARE @YeniSiparisID INT;
+    INSERT INTO Sipariş (Müsteri_ID, Siparis_Tarihi, Toplam_Tutar)
+    VALUES ('12345678901', '2024-12-21', '1000');
+    SET @YeniSiparisID = SCOPE_IDENTITY();
+
+    INSERT INTO Sipariş_Ürün (Siparis_ID, Parça_ID, Miktar, Toplam_Fiyat)
+    VALUES (@YeniSiparisID, 1, 2, 500);
+    INSERT INTO Sipariş_Ürün (Siparis_ID, Parça_ID, Miktar, Toplam_Fiyat)
+    VALUES (@YeniSiparisID, 2, 3, 500);
+
+    UPDATE Ürün
+    SET Stok_Miktari = Stok_Miktari - 2
+    WHERE Parca_ID = 330;
+    UPDATE Ürün
+    SET Stok_Miktari = Stok_Miktari - 3
+    WHERE Parca_ID = 347;
+
+    INSERT INTO Kargo (Kargo_Firma_ID, Takip_No, Durum, Siparis_ID)
+    VALUES (1, '10001', 'Hazırlanıyor', @YeniSiparisID);
+
+    COMMIT TRANSACTION;
+    PRINT 'Sipariş başarıyla tamamlandı.';
+END TRY
+
+BEGIN CATCH
+    ROLLBACK TRANSACTION;
+    PRINT 'Hata: Sipariş işlemi tamamlanamadı. Değişiklikler geri alındı.';
+END CATCH;
