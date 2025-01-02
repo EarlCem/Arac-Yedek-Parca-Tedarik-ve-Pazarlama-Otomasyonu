0af0d91725027c9c48f17170ee96c5fc5688b87a
EarlCem <57097066+EarlCem@users.noreply.github.com>
2025-01-02 16:24:16 +0300

    Add files via upload

---
+++ Stored Procedure/Stored Procedure-Ürün Ekleme.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE UrunKategorisiEkle
+    @UrunTuru VARCHAR(100),
+    @DepoID INT
+AS
+BEGIN
+    INSERT INTO Ürün_Kategorisi (Ürün_Türü, Bulunduğu_Depo)
+    VALUES (@UrunTuru, @DepoID);
+END;
+
+EXEC UrunKategorisiEkle @UrunTuru = 'Elektronik', @DepoID = 19;
